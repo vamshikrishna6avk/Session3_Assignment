51fb7b4a2b83046c3edc720abcb627f173e4b35b
vamshikrishna6avk <vamshikrishna6avk@gmail.com>
2017-06-14 14:58:17 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package assignment1;
+
+public class HelloWorld {
+	public static void main( String[] args ) {
+        System.out.println( "Hello World!");
+        System.exit( 0 ); //success
+    }
+
+}
